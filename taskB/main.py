fb6c703b9b4e17e65b2907b0b4c4fcfcd414b0a7
vudeam <62073166+vudeam@users.noreply.github.com>
2021-11-03 23:04:55 +0300

    Correct task B

--- taskB/main.py
+++ taskB/main.py
@@ -1,54 +1,70 @@
 from sys import stdin
+from collections import namedtuple
 
 
-def change(a: list, i, j) -> list:
-    a[i - 1] = a[i - 1] - a[j - 1]
-    return a
+Ingredient = namedtuple('Ingredient', ['name', 'count', 'symbol'])
 
-def medi(a: list) -> int:
-    c = a.copy()
-    c.sort()
-    return c[1]
 
-def index(a: list) -> list:
-    m = medi(a)
-    idx = []
-    for i, v in enumerate(a):
-        if v == m:
-            idx.append(i + 1)
-    return idx
+class Glass:
+    def __init__(self, n, m) -> None:
+        self.nlines = n
+        self.mchars = m
+        self.glass: list[str] = []
+        self.ingredients: list[Ingredient] = []
+
+    def read_glass(self) -> None:
+        if not self.nlines or not self.mchars:
+            raise ValueError('Params must be set')
+        for n in range(self.nlines):
+            self.glass.append(stdin.readline().rstrip('\n'))
+            # self.glass.append(stdin.readline().split())
+
+    def read_ingredients(self) -> list:
+        for i in range(int(input())):
+            l = stdin.readline().rstrip('\n').split(' ')
+            self.ingredients.append(
+                Ingredient(l[0], int(l[1]), l[2])
+            )
+        return self.ingredients
+
+    def fill_glass(self) -> None:
+        if len(self.ingredients) < 1:
+            raise ValueError('Input ingredients first')
+        off = 1
+        glass = []
+        for ing in self.ingredients:
+            for line in range(ing.count):
+                layer = self.glass[-(1 + off + line)].replace(' ', ing.symbol)
+                # self.glass[::-1][off + line] = layer
+                self.glass[-(1 + off + line)] = layer
+                #print(f'Filling with: {ing}; {layer}')
+                #print(f'Filled line: {self.glass[-(1 + off + line)]}')
+                # print(f'Filled line: {self.glass[::-1][off + line]}')
+                # off += 1
+            # off = ing.count + 1
+            off += ing.count
+
+    def __str__(self) -> None:
+        result = ''
+        for line in self.glass:
+            result += (line + '\n')
+        return result
+
 
 def main() -> None:
-    # a = [1, -3, 2]
-    # # print(change(a, 2, 1))
-    # # print(medi(a))
-    # # print(idx([3, 0, 3]))
-    a = [int(l) for l in stdin.readline().split(' ')]
-    finished = False
-    results = []
+    nm = stdin.readline()
+    n = int(nm.split(' ')[0])
+    m = int(nm.split(' ')[1])
 
-    for idx, val in enumerate(a):
-        # if (val + 1) in index(a):
-        if (idx + 1) in index(a): 
-            # print('YES')
-            results.append((idx, 'YES'))
-            continue
-        elif not finished:
-            for i in range(3):
-                for j in range(3):
-                    if (val + 1) in index(change(a, i + 1, j + 1)):
-                        # print('YES')
-                        results.append((idx, 'YES'))
-                        finished = True
-                    else:
-                        # print('NO')
-                        results.append((idx, 'NO'))
-                        finished = False
-            results.append((idx, 'NO'))
+    glass = Glass(n, m)
+    glass.read_glass()
+    #print(glass)
 
-    # print(results)
-    for i in range(3):
-        print(next(r for r in results if r[0] == i)[1])
+    glass.read_ingredients()
+
+    glass.fill_glass()
+    print(glass)
+
 
 if __name__ == '__main__':
     main()
